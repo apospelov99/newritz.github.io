deb05f0075c07e5caddc47a076c844d6cbe3805f
apospelov13 <small99@hotbox.ru>
2017-08-27 23:43:39 +0300

    v22

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 $(function() {
-
+  //alert("Hello");
+  $(".direction_thumbnail").matchHeight();
+  $(".gallery_col").matchHeight();
+  $(".project_thumbnail").matchHeight();
+  $(".project_thumbnail .gallery_title").matchHeight();
+  $(".project_thumbnail .clientcards_gallery").matchHeight();
+  //$.each(".project_thumbnail",".project_thumbnail .gallery_title").matchHeight();
 });
