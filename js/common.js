bded1cf3c7dc352e96247326469db5b714f10ec3
apospelov13 <small99@hotbox.ru>
2017-08-28 22:55:59 +0300

    v23

--- js/common.js
+++ js/common.js
@@ -5,5 +5,7 @@ $(function() {
   $(".project_thumbnail").matchHeight();
   $(".project_thumbnail .gallery_title").matchHeight();
   $(".project_thumbnail .clientcards_gallery").matchHeight();
+  $(".offer_col .offer_text").matchHeight();
+  $(".thumbnail_wrap").matchHeight();
   //$.each(".project_thumbnail",".project_thumbnail .gallery_title").matchHeight();
 });
